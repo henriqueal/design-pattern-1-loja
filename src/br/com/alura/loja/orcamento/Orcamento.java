19b1721005a6366ea49be817e139fbd125e7acd9
Henrique Lima <henrique.lima@valecard.com.br>
2022-06-27 20:18:31 -0300

    Pre Design Pattern: State

--- src/br/com/alura/loja/orcamento/Orcamento.java
+++ src/br/com/alura/loja/orcamento/Orcamento.java
@@ -6,10 +6,25 @@ public class Orcamento {
 
     private BigDecimal valor;
     private int quantidadeItens;
+    private String situacao;
 
     public Orcamento(BigDecimal valor, int quantidadeItens) {
         this.valor = valor;
         this.quantidadeItens = quantidadeItens;
+    }
+
+    public getDescontoExtra(){
+        BigDecimal valorDescontoExtra = BigDecimal.ZERO;
+        if(situacao.equals("EM_ANALISE")){
+            valorDescontoExtra = new BigDecimal("0.05");
+        } else if(situacao.equals("APROVADO")){
+            valorDescontoExtra = new BigDecimal("0.02");
+        }
+        this.valor = this.valor.subtract(valorDescontoExtra);
+    }
+
+    public void aprovar(){
+        this.situacao = "APROVADO";
     }
 
     public BigDecimal getValor() {
