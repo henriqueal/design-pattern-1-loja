5080569295cb1a41e93d6376ea7acf16ed743352
Henrique Lima <henrique.lima@valecard.com.br>
2022-08-15 17:59:19 -0300

    Padrão Observer

--- src/br/com/alura/loja/TestesPedidos.java
+++ src/br/com/alura/loja/TestesPedidos.java
@@ -1,19 +1,25 @@
 package br.com.alura.loja;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import br.com.alura.loja.pedido.GeraPedido;
 import br.com.alura.loja.pedido.GeraPedidoHandler;
+import br.com.alura.loja.pedido.acao.EnviaEmailAcao;
+import br.com.alura.loja.pedido.acao.SalvaPedidoNoBancaoAcao;
 
 public class TestesPedidos {
-	
+
 	public static void main(String[] args) {
 		String cliente = "Ana da Silva";
 		BigDecimal valorOrcamento = new BigDecimal("745.99");
 		int quantidadeItens = 3;
 		
 		GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
-		GeraPedidoHandler handler = new GeraPedidoHandler();
+		GeraPedidoHandler handler = new GeraPedidoHandler(
+				Arrays.asList(new SalvaPedidoNoBancaoAcao(),
+						new EnviaEmailAcao())
+		);
 		handler.executar(gerador);
 	}
 
--- src/br/com/alura/loja/pedido/acao/EnviaEmailAcao.java
+++ src/br/com/alura/loja/pedido/acao/EnviaEmailAcao.java
@@ -2,7 +2,7 @@ package br.com.alura.loja.pedido.acao;
 
 import br.com.alura.loja.pedido.Pedido;
 
-public class EnviaEmailAcao {
+public class EnviaEmailAcao implements AcaoAposGerarPedido{
     public void executar(Pedido pedido){
         System.out.println("Enviando email para cliente sobre pedido...");
     }
